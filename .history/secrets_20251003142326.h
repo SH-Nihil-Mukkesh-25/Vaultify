97d38037a91fe9627da209b0d08095ae20aa901b
Nihil <mukkeshnihil@gmail.com>
2025-10-03 14:23:57 +0530

    new deployment

---
+++ .history/secrets_20251003142326.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// -------------------- WIFI --------------------
+const char* WIFI_SSID = "YOUR_WIFI_SSID";
+const char* WIFI_PASS = "YOUR_WIFI_PASS";
+
+// -------------------- TWILIO --------------------
+const String TWILIO_ACCOUNT_SID = "YOUR_TWILIO_SID";
+const String TWILIO_AUTH_TOKEN  = "YOUR_TWILIO_TOKEN";
+const String DEST_PHONE = "+91XXXXXXXXXX";
+const String TWILIO_FROM_NUMBER = "+1XXXXXXXXXX";
+
+// -------------------- BACKEND --------------------
+const String BACKEND_URL = "https://vaultify-1.onrender.com"; // ESP32 -> backend
